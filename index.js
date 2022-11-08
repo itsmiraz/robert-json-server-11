0ebb20bb1d13b1be95b7478c50ee9b1120fcff01
itsmiraz <mirajulislamofficial044@gmail.com>
2022-11-08 10:03:39 +0600

    run  function

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const app = express()
-const port = process.env.PORT || 5000
-app.use(cors())
-
-// username : robertjsonAss
-// password : Wpd3zZYQHEaJgkZZ
-
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.fpgnyx0.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
+const port = process.env.PORT || 5000
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.fpgnyx0.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+
+}
+
+run().catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
