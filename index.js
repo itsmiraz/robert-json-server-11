c4c088205b235c93108e24532b324b9debb72431
itsmiraz <mirajulislamofficial044@gmail.com>
2022-11-08 14:38:04 +0600

    getting single service

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express()
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000
 app.use(cors())
@@ -16,6 +16,28 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 async function run() {
+
+    try {
+
+        const serviceCollection = client.db('RobertJsonService').collection('services')
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await serviceCollection.findOne(query)
+            res.send(result)
+        })
+
+    }
+    finally {
+
+    }
 
 }
 
