a3c6afe993792f1761f3fc5af390359283a8d5da
itsmiraz <mirajulislamofficial044@gmail.com>
2022-11-08 10:01:39 +0600

    mongo added

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const app = express()
 const port = process.env.PORT || 5000
 app.use(cors())
 
+// username : robertjsonAss
+// password : Wpd3zZYQHEaJgkZZ
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<username>:<password>@cluster0.fpgnyx0.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+
 app.get('/', (req, res) => {
     res.send('Server Is Runnig')
 })
